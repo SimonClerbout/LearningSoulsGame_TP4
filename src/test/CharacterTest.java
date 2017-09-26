1003ab6b00b1fb00b4363638af8e926ab9fac2a8
Eric Ramat <eric.ramat@gmail.com>
2017-09-26 11:56:13 +0200

    TP3: q1.1

---
+++ src/test/CharacterTest.java
@@ -0,0 +1,68 @@
+package test;
+
+import com.sun.istack.internal.Nullable;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.*;
+
+public class CharacterTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Nullable
+    private Constructor<?> searchDefaultConstructor(Class<?> c) {
+        for (Constructor<?> constructor : c.getConstructors()) {
+            if (constructor.getParameterCount() == 0) {
+                return constructor;
+            }
+        }
+        return null;
+    }
+
+    @Test
+    public void testAttributes() {
+        try {
+            Class<?> c = Class.forName("lsg.characters.Monster");
+            Field f = c.getDeclaredField("skinThickness");
+
+            Assert.assertEquals(f.getModifiers(), Modifier.PRIVATE);
+            Assert.assertEquals(f.getType(), float.class);
+
+            Constructor<?> constructor = searchDefaultConstructor(c);
+            Object o = constructor.newInstance();
+
+            f.setAccessible(true);
+
+            Assert.assertEquals((float) (f.get(o)), 20f, 0f);
+
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called Monster");
+        } catch (NoSuchFieldException e) {
+            Assert.fail("should have an attribute named skinThickness");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
