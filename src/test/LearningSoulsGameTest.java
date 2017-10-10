1a66aeb136be96a6f79f3885e4dc29ddd7a85234
Eric Ramat <eric.ramat@gmail.com>
2017-10-10 10:56:17 +0200

    TP3: q4.7

---
+++ src/test/LearningSoulsGameTest.java
@@ -0,0 +1,68 @@
+package test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.*;
+
+public class LearningSoulsGameTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final ByteArrayInputStream in = new ByteArrayInputStream("\n\n\n".getBytes());
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+        System.setIn(in);
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+        System.setIn(null);
+    }
+
+    @Test
+    public void testMain() {
+        try {
+            Class<?> c = Class.forName("lsg.LearningSoulsGame");
+            Method m = c.getMethod("main", String[].class);
+            Object[] args = new Object[1];
+
+            args[0] = new String[]{};
+            m.invoke(null, args);
+
+            String[] list = outContent.toString().split("\n");
+
+            Assert.assertEquals("[ Hero ]             Gregooninator        LIFE:  100      STAMINA:   50      PROTECTION:0.0       (ALIVE)", list[0]);
+            Assert.assertEquals("[ Monster ]          Monster_1            LIFE:   10      STAMINA:   10      PROTECTION:20.0      (ALIVE)", list[1]);
+            Assert.assertEquals("", list[2]);
+            Assert.assertEquals("Hit enter key for next move > ", list[3]);
+            Assert.assertEquals("Gregooninator attacks Monster_1 with Basic Sword (ATTACK:9 | DMG : 7)", list[4]);
+            Assert.assertEquals("[ Hero ]             Gregooninator        LIFE:  100      STAMINA:   30      PROTECTION:0.0       (ALIVE)", list[5]);
+            Assert.assertEquals("[ Monster ]          Monster_1            LIFE:    3      STAMINA:   10      PROTECTION:20.0      (ALIVE)", list[6]);
+            Assert.assertEquals("", list[7]);
+            Assert.assertEquals("Hit enter key for next move > ", list[8]);
+            Assert.assertEquals("Monster_1 attacks Gregooninator with Bloody Claw (ATTACK:146 | DMG : 100)", list[9]);
+            Assert.assertEquals("[ Hero ]             Gregooninator        LIFE:    0      STAMINA:   30      PROTECTION:0.0       (DEAD)", list[10]);
+            Assert.assertEquals("[ Monster ]          Monster_1            LIFE:    3      STAMINA:    5      PROTECTION:20.0      (ALIVE)", list[11]);
+            Assert.assertEquals("", list[12]);
+            Assert.assertEquals("--- Monster_1 WINS !!! ---", list[13]);
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called LearningSoulsGame");
+        } catch (NoSuchMethodException e) {
+            Assert.fail("should have a static method called main");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
