9133be9f4e75302197a7c92f6fa8ecac85b4e548
Eric Ramat <eric.ramat@gmail.com>
2017-10-18 09:37:23 +0200

    TP4: q2.2

--- src/test/ConsumableTest.java
+++ src/test/ConsumableTest.java
@@ -154,4 +154,56 @@ public class ConsumableTest {
         }
     }
 
+    @Test
+    public void existSubClasses() {
+        try {
+            Class<?> c1 = Class.forName("lsg.consumables.drinks.Coffee");
+            Class<?> c2 = Class.forName("lsg.consumables.drinks.Drink");
+            Class<?> c3 = Class.forName("lsg.consumables.drinks.Whisky");
+            Class<?> c4 = Class.forName("lsg.consumables.drinks.Wine");
+            Class<?> c5 = Class.forName("lsg.consumables.Consumable");
+
+            Assert.assertTrue("Drink should be a subclass of Consumable", c5.isAssignableFrom(c2));
+            Assert.assertTrue("Coffee should be a subclass of Drink", c2.isAssignableFrom(c1));
+            Assert.assertTrue("Whisky should be a subclass of Drink", c2.isAssignableFrom(c3));
+            Assert.assertTrue("Wine should be a subclass of Drink", c2.isAssignableFrom(c4));
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have subclasses of Consumable");
+        }
+    }
+
+    @Test
+    public void testDrinksToString() {
+        try {
+            Class<?> c0 = Class.forName("lsg.consumables.Consumable");
+            Class<?> c1 = Class.forName("lsg.consumables.drinks.Coffee");
+            Class<?> c2 = Class.forName("lsg.consumables.drinks.Whisky");
+            Class<?> c3 = Class.forName("lsg.consumables.drinks.Wine");
+
+            Constructor<?> constructor1 = c1.getDeclaredConstructor();
+            Constructor<?> constructor2 = c2.getDeclaredConstructor();
+            Constructor<?> constructor3 = c3.getDeclaredConstructor();
+
+            Object o1 = constructor1.newInstance();
+            Object o2 = constructor2.newInstance();
+            Object o3 = constructor3.newInstance();
+
+            Method m = c0.getDeclaredMethod("toString");
+
+            Assert.assertEquals("Hot Grandmother Coffee [10 stamina point(s)]", (String) (m.invoke(o1)));
+            Assert.assertEquals("12 years old Oban [150 stamina point(s)]", (String) (m.invoke(o2)));
+            Assert.assertEquals("Pomerol 2008 [30 stamina point(s)]", (String) (m.invoke(o3)));
+        } catch (ClassNotFoundException e) {
+            Assert.fail("should have a class called Consumable and subclasses in consumables package");
+        } catch (NoSuchMethodException e) {
+            Assert.fail("should have a method called toString in Consumable class");
+        } catch (IllegalAccessException e) {
+            Assert.fail("IllegalAccessException");
+        } catch (InstantiationException e) {
+            Assert.fail("InstantiationException");
+        } catch (InvocationTargetException e) {
+            Assert.fail("InvocationTargetException");
+        }
+    }
+
 }
